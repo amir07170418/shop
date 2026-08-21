6b453695ec698e634fb3c5ea799f759b723cbeef
amir07170418 <hossein13810418@gmail.com>
2026-08-21 11:30:13 +0330

    repositories updated

--- src/main/java/org/example/shop/repository/ProductRepository.java
+++ src/main/java/org/example/shop/repository/ProductRepository.java
@@ -2,8 +2,13 @@ package org.example.shop.repository;
 
 import org.example.shop.model.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface ProductRepository extends JpaRepository<Product, Long> {
+    boolean existsByName(String name);
+    @Query("select count(p)>0 from Product p join p.orderItems i where i.product.id=:id")
+    boolean existsInOrderItemsById(@Param("id") Long id);
 }
--- src/main/java/org/example/shop/repository/ReviewRepository.java
+++ src/main/java/org/example/shop/repository/ReviewRepository.java
@@ -2,8 +2,14 @@ package org.example.shop.repository;
 
 import org.example.shop.model.Review;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface ReviewRepository extends JpaRepository<Review, Long> {
+    @Query("select count(r)>0 from Review r where r.product.id=:productId and r.customer.id=:customerId")
+    boolean existsByProductIdAndCustomerId(Long productId, Long customerId);
+    @Query("select count(item)>0 from OrderItem item where item.product.id=:productId and item.order.customer.id=:customerId " +
+            "and item.order.payment.status=org.example.shop.model.PaymentStatus.SUCCESS")
+    boolean checkBuyProduct(Long productId, Long customerId);
 }
